7a85fe0efa068f058d0e9f1a94b76c80cdafeb9d
shahedsuleiman <shahedsuleiman2001@gmail.com>
2024-01-29 16:22:47 +0300

    hiiiiiiiiiiii

--- my-portfolio/index.js
+++ my-portfolio/index.js
@@ -1,60 +1,65 @@
-import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
-import * as dat from 'dat.gui';
+import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js";
+import * as dat from "dat.gui";
 console.log(dat);
-
 
 const gui = new dat.GUI();
 const world = {
-   plane:{
-    width:10,
+  plane: {
+    width: 10,
+  },
+};
+gui.add(world.plane, "width", 1, 500).onChange(() => {
+  planeMesh.geometry.dispose();
+  planeMesh.geometry = new THREE.PlaneGeometry(world.plane.width, 10, 10, 10);
+  const { array } = planeMesh.geometry.attributes.position;
+  for (let i = 0; i < array.length; i += 3) {
+    const x = array[i];
+    const y = array[i + 1];
+    const z = array[i + 2];
 
-   }
-}
-gui.add(world.plane,'width', 1,500).onChange ( () => {
-    planeMesh.geometry.dispose();
-    planeMesh.geometry = new THREE.PlaneGeometry(world.plane.width,10,10,10);
-})
+    array[i + 2] = z + Math.random();
+  }
+});
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); //4 arguments [field of view in degrees,aspect ratio of the scene,clipping planes (how close the object is to the camera before its clipped),far clippping planes]
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+); //4 arguments [field of view in degrees,aspect ratio of the scene,clipping planes (how close the object is to the camera before its clipped),far clippping planes]
 
 const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(innerWidth, innerHeight);
 renderer.setPixelRatio(devicePixelRatio);
 document.body.appendChild(renderer.domElement);
- //boxgeometry[width,length,height]
+//boxgeometry[width,length,height]
 
+camera.position.z = 5;
 
-camera.position.z= 5;
-
-const planeGeometry = new THREE.PlaneGeometry(10,10,10,10);
-const planeMaterial = new THREE.MeshPhongMaterial({color:0xff0000,
- side: THREE.DoubleSide,
-flatShading: THREE.FlatShading
+const planeGeometry = new THREE.PlaneGeometry(10, 10, 10, 10);
+const planeMaterial = new THREE.MeshPhongMaterial({
+  color: 0xff0000,
+  side: THREE.DoubleSide,
+  flatShading: THREE.FlatShading,
 });
-const planeMesh = new THREE.Mesh(planeGeometry,planeMaterial);
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(planeMesh);
-const {array} =planeMesh.geometry.attributes.position;
-for(let i = 0; i<array.length; i +=3 ){
-    const x = array[i];
-    const y = array[i+1];
-    const z= array[i+2];
+const { array } = planeMesh.geometry.attributes.position;
+for (let i = 0; i < array.length; i += 3) {
+  const x = array[i];
+  const y = array[i + 1];
+  const z = array[i + 2];
 
-    array[i + 2] = z + Math.random()
+  array[i + 2] = z + Math.random();
 }
-const light = new THREE.DirectionalLight(
-   0xffffff,1 
-)
-light.position.set(0,0,1);
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 0, 1);
 scene.add(light);
-function animate(){
-    requestAnimationFrame(animate)
-    renderer.render(scene,camera);
-    // planeMesh.rotation.x += 0.01;
-    
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  // planeMesh.rotation.x += 0.01;
 }
 
 animate();
-
-
-
