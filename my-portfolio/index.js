2ad55a7e1ee169c2d5472554f386ec5713deb694
shahedsuleiman <shahedsuleiman2001@gmail.com>
2024-01-29 14:26:50 +0300

    hello

--- my-portfolio/index.js
+++ my-portfolio/index.js
@@ -9,25 +9,22 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(innerWidth, innerHeight);
 renderer.setPixelRatio(devicePixelRatio);
 document.body.appendChild(renderer.domElement);
-const boxGeometry = new THREE.BoxGeometry(1,1,1); //[width,length,height]
-const material = new THREE.MeshBasicMaterial({color:0x00FF00});
-const mesh = new THREE.Mesh(boxGeometry,material)
+ //boxgeometry[width,length,height]
 
-scene.add(mesh);
+
 camera.position.z= 5;
 
 const planeGeometry = new THREE.PlaneGeometry(5,5,10,10);
-const planeMaterial = new THREE.MeshBasicMaterial({color:0xff0000});
+const planeMaterial = new THREE.MeshBasicMaterial({color:0xff0000,
+ side: THREE.DoubleSide});
 const planeMesh = new THREE.Mesh(planeGeometry,planeMaterial);
 scene.add(planeMesh);
 
 function animate(){
     requestAnimationFrame(animate)
     renderer.render(scene,camera);
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.01;
     planeMesh.rotation.x += 0.01;
-    planeMesh.rotation.y += 0.01;
+    
 }
 
 animate();
