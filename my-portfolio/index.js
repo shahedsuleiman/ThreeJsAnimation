89501360ea6f5ad0bb7d8822c289e652caf5d648
shahedsuleiman <shahedsuleiman2001@gmail.com>
2024-01-29 14:11:39 +0300

    animation

--- my-portfolio/index.js
+++ my-portfolio/index.js
@@ -16,12 +16,18 @@ const mesh = new THREE.Mesh(boxGeometry,material)
 scene.add(mesh);
 camera.position.z= 5;
 
-const planeGeometry = new THREE.PlaneGeometry(1,1,1);
+const planeGeometry = new THREE.PlaneGeometry(5,5,10,10);
+const planeMaterial = new THREE.MeshBasicMaterial({color:0xff0000});
+const planeMesh = new THREE.Mesh(planeGeometry,planeMaterial);
+scene.add(planeMesh);
+
 function animate(){
     requestAnimationFrame(animate)
     renderer.render(scene,camera);
     mesh.rotation.x += 0.01;
     mesh.rotation.y += 0.01;
+    planeMesh.rotation.x += 0.01;
+    planeMesh.rotation.y += 0.01;
 }
 
 animate();
